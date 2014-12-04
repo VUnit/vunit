1367cc81ebf131722d4c59ad03e41a5a76cfe0cd
Lars Asplund <lars.asplund@autoliv.com>
2014-12-04 15:10:40 +0100

    Made the default run.py script a more basic example. Other more advanced features which may or may not be needed/wanted in various situations will be added/described in separate run.py files.

--- examples/uart/run.py
+++ examples/uart/run.py
@@ -12,27 +12,15 @@ sys.path.append(path_to_vunit)
 #  -------
 
 from vunit import VUnit
-from vunit.csv_logs import CsvLogs
-
-def post_merge_csv():
-    def merge_csv(output_path):
-        CsvLogs(join(output_path, "*.csv")).write(join(output_path,"merged_logs.csv"))
-        return True
-    return merge_csv
-
-src_path = join(dirname(__file__), "src")
 
 ui = VUnit.from_argv()
-ui.enable_location_preprocessing(additional_subprograms=["check_received_bytes"])
+
+src_path = join(dirname(__file__), "src")
 
 uart_lib = ui.add_library("uart_lib")
 uart_lib.add_source_files(join(src_path, "*.vhd"))
 
 tb_uart_lib = ui.add_library("tb_uart_lib")
 tb_uart_lib.add_source_files(join(src_path, "test", "*.vhd"))
-tb_uart_rx = tb_uart_lib.entity("tb_uart_rx")
-tb_uart_tx = tb_uart_lib.entity("tb_uart_tx")
-tb_uart_rx.add_config(name="merge_csv", generics = dict(), post_check=post_merge_csv())
-tb_uart_tx.add_config(name="merge_csv", generics = dict(), post_check=post_merge_csv())
 
 ui.main()
