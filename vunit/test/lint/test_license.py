86f4e6c4e2f823c9ce1dce3bca7651966871e088
Olof Kraigher <olof.kraigher@gmail.com>
2015-05-20 16:56:44 +0200

    Added check that there is no trailing whitespace in non-python files

--- vunit/test/lint/test_license.py
+++ vunit/test/lint/test_license.py
@@ -7,7 +7,7 @@
 """
 License header sanity check
 """
-
+from __future__ import print_function
 
 import unittest
 from os.path import join, splitext, abspath, commonprefix
@@ -40,11 +40,16 @@ class TestLicense(unittest.TestCase):
                 osvvm_integration_example_directory = abspath(join(ROOT, 'examples', 'osvvm_integration', 'src'))
                 if is_prefix_of(osvvm_integration_example_directory, abspath(join(root, file_name))):
                     continue
-                if splitext(file_name)[1] in ['.vhd', '.vhdl', '.py', '.v', '.sv']:
+                if splitext(file_name)[1] in ('.vhd', '.vhdl', '.py', '.v', '.sv'):
                     licensed_files.append(join(root, file_name))
 
         for file_name in licensed_files:
-            self._check_license(file_name)
+            code = ostools.read_file(file_name)
+
+            self._check_license(code, file_name)
+
+            if splitext(file_name)[1] in ('.vhd', '.vhdl', '.v', '.sv'):
+                self._check_no_trailing_whitespace(code, file_name)
 
     _re_license_notice = re.compile(
         r"(?P<comment_start>#|--|//) This Source Code Form is subject to the terms of the Mozilla Public" + "\n"
@@ -55,7 +60,7 @@ class TestLicense(unittest.TestCase):
         r"Lars Asplund lars\.anders\.asplund@gmail\.com")
     _re_log_date = re.compile(r'Date:\s*(?P<year>20\d\d)-\d\d-\d\d')
 
-    def _check_license(self, file_name):
+    def _check_license(self, code, file_name):
         """
         Check that the license header of file_name is valid
         """
@@ -73,7 +78,6 @@ class TestLicense(unittest.TestCase):
             first_year = datetime.now().year
             last_year = first_year
 
-        code = ostools.read_file(file_name)
         match = self._re_license_notice.search(code)
         self.assertIsNotNone(match, "Failed to find license notice in %s" % file_name)
         if first_year == last_year:
@@ -87,6 +91,25 @@ class TestLicense(unittest.TestCase):
                              'Expected copyright year range to start with %d in %s' % (first_year, file_name))
             self.assertEqual(int(match.group('last_year')), last_year,
                              'Expected copyright year range to end with %d in %s' % (last_year, file_name))
+
+    @staticmethod
+    def _check_no_trailing_whitespace(code, file_name):
+        """
+        Check that there is no trailing whitespace within the code
+        """
+        for idx, line in enumerate(code.splitlines()):
+            sline = line.rstrip()
+            if sline == line:
+                continue
+            line_prefix = "%i: " % (idx + 1)
+            print("Trailing whitespace violation in %s" % file_name)
+            print(line_prefix + line)
+            for _ in range(len(line_prefix) + len(sline)):
+                print(" ", end="")
+            for _ in range(len(line) - len(sline)):
+                print("~", end="")
+            print()
+            raise AssertionError("Line %i of %s contains trailing whitespace", idx + 1, file_name)
 
 
 def is_prefix_of(prefix, of_path):
