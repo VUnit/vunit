1b7f043d4502ec62af56f8c0c3b12bbf3efc9acd
Olof Kraigher <olof.kraigher@gmail.com>
2015-06-06 09:09:12 +0200

    Update persistent database version due to changing vhdl parse results.

--- vunit/ui.py
+++ vunit/ui.py
@@ -148,7 +148,7 @@ class VUnit(object):  # pylint: disable=too-many-instance-attributes, too-many-p
         project_database_file_name = join(self._output_path, "project_database")
         create_new = False
         key = b"version"
-        version = str((3, sys.version)).encode()
+        version = str((4, sys.version)).encode()
         database = None
         try:
             database = DataBase(project_database_file_name)
