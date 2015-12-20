cbbe373bc3428cf710e415b188383b19b16a13b8
kraigher <olof.kraigher@gmail.com>
2015-12-20 09:04:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ data_files = [os.path.relpath(file_name, 'vunit') for file_name in data_files]
 
 setup(
     name='vunit_hdl',
-    version='0.48.1',
+    version='0.48.2',
     packages=['vunit',
               'vunit.com',
               'vunit.test',
