218e15dfc83a290caa5331ae11e6b2d95c707803
Olof Kraigher <olof.kraigher@gmail.com>
2016-03-03 19:14:49 +0100

    Added python version check. Closes #141.

--- vunit/__init__.py
+++ vunit/__init__.py
@@ -9,6 +9,7 @@ Public VUnit interface
 """
 
 from os.path import dirname, join, abspath
+import vunit.version_check
 from vunit.ui import VUnit
 from vunit.vunit_cli import VUnitCLI
 
---
+++ vunit/version_check.py
@@ -0,0 +1,31 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2016, Lars Asplund lars.anders.asplund@gmail.com
+
+"""
+Check that the Python version used is supported by VUnit
+"""
+
+from __future__ import print_function
+import sys
+
+
+def version_is_ok():
+    """
+    Returns true if version is 2.7 or higher or equal to 3.3
+    """
+    version = (sys.version_info.major,
+               sys.version_info.minor)
+    return version == (2, 7) or version >= (3, 3)
+
+
+if not version_is_ok():
+    print("Your Python version (%i.%i) is too old for VUnit. "
+          "Please consider upgrading." % (sys.version_info.major,
+                                          sys.version_info.minor))
+    print("VUnit supports versions:")
+    print(" - Python 2.7")
+    print(" - Python 3.3 or higher")
+    exit(1)
