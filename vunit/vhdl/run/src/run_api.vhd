58612beae8304321ca4d9f69f1942b5cf646edcb
Olof Kraigher <olof.kraigher@gmail.com>
2016-03-19 17:44:02 +0100

    Copyright fix

--- vunit/vhdl/run/src/run_api.vhd
+++ vunit/vhdl/run/src/run_api.vhd
@@ -5,7 +5,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this file,
 -- You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- Copyright (c) 2014-2015, Lars Asplund lars.anders.asplund@gmail.com
+-- Copyright (c) 2014-2016, Lars Asplund lars.anders.asplund@gmail.com
 
 use work.check_types_pkg.checker_stat_t;
 use work.run_base_pkg.all;
