e46e7158c549b40babd27f60fcf7b4b59ca98aff
Andreas Ehliar <ehliar@isy.liu.se>
2016-04-07 10:20:07 +0200

    Added some documentation that would have been helpful to me

--- examples/verilog/user_guide/tb_example.sv
+++ examples/verilog/user_guide/tb_example.sv
@@ -4,12 +4,20 @@
 //
 // Copyright (c) 2015-2016, Lars Asplund lars.anders.asplund@gmail.com
 
+// You do not need to worry about adding vunit_defines.svh to your
+// include path, VUnit will automatically do that for you if your
+// python run-script is correct.
 `include "vunit_defines.svh"
 
 module tb_example;
    `TEST_SUITE begin
-
+      // Note: Do not place any code here (unless you are debuggnig
+      // VUnit internals).
+      
       `TEST_SUITE_SETUP begin
+         // Here you will typically place things that are common to
+         // all tests, such as asserting the reset signal, starting
+         // the clock.
          $display("test suite setup");
       end
 
@@ -18,14 +26,17 @@ module tb_example;
       end
 
       `TEST_CASE("Test that pass") begin
-         $display("pass");
+         $display("this test is expected to pass");
+         `CHECK_EQUAL(1, 1);
       end
 
       `TEST_CASE("Test that fail") begin
-         `CHECK_EQUAL(0, 1);
+         $display("this test is expected to fail");
+         `CHECK_EQUAL(0, 1, "You may also optionally add a diagnostic message to CHECK_EQUAL");
       end
 
       `TEST_CASE("Test that timeouts") begin
+         $display("this test is expected to timeout");
          #2ns;
       end
 
@@ -38,5 +49,7 @@ module tb_example;
       end
    end;
 
+   // The watchdog macro is optional, but recommended. If you use it
+   // it must not be placed inside any initial or always-block
    `WATCHDOG(1ns);
 endmodule
