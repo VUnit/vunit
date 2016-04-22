a3259e5c3e2661a11fb9ae71f0919ac4abc56cd7
Andreas Ehliar <ehliar@isy.liu.se>
2016-04-22 09:49:55 +0200

    Use an assert for an internal error rather than a printout that should never happen.

--- vunit/test_report.py
+++ vunit/test_report.py
@@ -158,13 +158,10 @@ class TestReport(object):
             self._printer.write("All passed!", fg='gi')
         self._printer.write("\n")
 
+        assert len(all_tests) <= self._expected_num_tests
         if len(all_tests) < self._expected_num_tests:
             self._printer.write("WARNING: Expected to run %d tests, but only ran %d tests"
                                 % (self._expected_num_tests, len(all_tests)), fg='rgi')
-            self._printer.write("\n")
-        elif len(all_tests) > self._expected_num_tests:
-            self._printer.write("ERROR: Ran more tests than expected (%d > %d)" %
-                                (len(all_tests), self._expected_num_tests), fg='ri')
             self._printer.write("\n")
 
     def _split(self):
