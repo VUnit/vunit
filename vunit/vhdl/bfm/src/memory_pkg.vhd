2bb1030b40298ac5993417d5cbf7584a9cafd22a
Olof Kraigher <olof.kraigher@gmail.com>
2017-06-18 14:06:43 +0200

    Added memory model

---
+++ vunit/vhdl/bfm/src/memory_pkg.vhd
@@ -0,0 +1,199 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this file,
+-- You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Copyright (c) 2017, Lars Asplund lars.anders.asplund@gmail.com
+
+use work.integer_vector_ptr_pkg.all;
+use work.string_ptr_pkg.all;
+
+package memory_pkg is
+
+  type memory_t is record
+    -- Private
+    p_data : integer_vector_ptr_t;
+    p_perm : integer_vector_ptr_t;
+    p_allocs : integer_vector_ptr_t;
+  end record;
+  constant null_memory : memory_t := (others => null_ptr);
+
+  type alloc_t is record
+    -- Private
+    p_name : string_ptr_t;
+    p_address : natural;
+    p_num_bytes : natural;
+  end record;
+  constant null_alloc : alloc_t := (p_name => null_string_ptr, p_address => natural'low, p_num_bytes => natural'low);
+
+  type permissions_t is (no_access, write_only, read_only, read_and_write);
+
+  subtype byte_t is integer range 0 to 255;
+
+  impure function new_memory return memory_t;
+  impure function allocate(memory : memory_t; num_bytes : natural; name : string := ""; alignment : positive := 1) return alloc_t;
+  procedure write_byte(memory : memory_t; address : natural; byte : byte_t);
+  procedure write_byte(memory : memory_t; address : natural; byte : byte_t; variable error_msg : inout string_ptr_t);
+  procedure read_byte(memory : memory_t; address : natural; variable byte : inout byte_t; variable error_msg : inout string_ptr_t);
+  impure function read_byte(memory : memory_t; address : natural) return byte_t;
+  impure function get_permissions(memory : memory_t; address : natural) return permissions_t;
+  procedure set_permissions(memory : memory_t; address : natural; permissions : permissions_t);
+  impure function describe_address(memory : memory_t; address : natural) return string;
+
+  impure function base_address(alloc : alloc_t) return natural;
+  impure function last_address(alloc : alloc_t) return natural;
+
+end package;
+
+package body memory_pkg is
+  impure function new_memory return memory_t is
+  begin
+    return (p_data => allocate(0), p_perm => allocate(0), p_allocs => allocate(0));
+  end;
+
+  impure function allocate(memory : memory_t; num_bytes : natural; name : string := ""; alignment : positive := 1) return alloc_t is
+    variable alloc : alloc_t;
+  begin
+    alloc.p_name := allocate(name);
+    alloc.p_address := length(memory.p_data);
+    alloc.p_address := alloc.p_address + ((-alloc.p_address) mod alignment);
+    alloc.p_num_bytes := num_bytes;
+    resize(memory.p_data, last_address(alloc) + 1);
+    resize(memory.p_perm, length(memory.p_data));
+    resize(memory.p_allocs, length(memory.p_allocs)+3, value => permissions_t'pos(no_access));
+
+    set(memory.p_allocs, length(memory.p_allocs)-3, to_integer(alloc.p_name));
+    set(memory.p_allocs, length(memory.p_allocs)-2, alloc.p_address);
+    set(memory.p_allocs, length(memory.p_allocs)-1, alloc.p_num_bytes);
+
+    -- Set default access type
+    for i in 0 to num_bytes-1 loop
+      set(memory.p_perm, alloc.p_address + i, permissions_t'pos(read_and_write));
+    end loop;
+    return alloc;
+  end function;
+
+  impure function base_address(alloc : alloc_t) return natural is
+  begin
+    return alloc.p_address;
+  end function;
+
+  impure function last_address(alloc : alloc_t) return natural is
+  begin
+    return alloc.p_address + alloc.p_num_bytes - 1;
+  end function;
+
+  impure function address_to_allocation(memory : memory_t; address : natural) return alloc_t is
+    variable alloc : alloc_t;
+  begin
+    -- @TODO use bisection for speedup
+    for i in 0 to length(memory.p_allocs)/3-1 loop
+      alloc.p_address := get(memory.p_allocs, 3*i+1);
+
+      if address >= alloc.p_address then
+        alloc.p_num_bytes := get(memory.p_allocs, 3*i+2);
+
+        if address < alloc.p_address + alloc.p_num_bytes then
+          alloc.p_name := to_string_ptr(get(memory.p_allocs, 3*i));
+          return alloc;
+        end if;
+      end if;
+    end loop;
+
+    return null_alloc;
+  end;
+
+  procedure check_address(memory : memory_t; address : natural;
+                          reading : boolean;
+                          variable error_msg : inout string_ptr_t) is
+    impure function verb return string is
+    begin
+      if reading then
+        return "Reading from";
+      else
+        return "Writing to";
+      end if;
+    end function;
+  begin
+    if length(memory.p_data) = 0 then
+      error_msg := allocate(verb & " empty memory");
+    elsif address >= length(memory.p_data) then
+      error_msg := allocate(verb & " memory out of range 0 to " & to_string(length(memory.p_data)-1));
+    elsif get_permissions(memory, address) = no_access then
+      error_msg := allocate(verb & " " & describe_address(memory, address) & " without permission (no_access)");
+    elsif reading and get_permissions(memory, address) = write_only then
+      error_msg := allocate(verb & " " & describe_address(memory, address) & " without permission (write_only)");
+    elsif not reading and get_permissions(memory, address) = read_only then
+      error_msg := allocate(verb & " " & describe_address(memory, address) & " without permission (read_only)");
+    else
+      error_msg := null_string_ptr;
+    end if;
+  end;
+
+  procedure write_byte(memory : memory_t; address : natural; byte : byte_t; variable error_msg : inout string_ptr_t) is
+  begin
+    check_address(memory, address, false, error_msg);
+    if error_msg /= null_string_ptr then
+      return;
+    end if;
+    set(memory.p_data, address, byte);
+  end;
+
+  procedure write_byte(memory : memory_t; address : natural; byte : byte_t) is
+    variable error_msg : string_ptr_t := null_string_ptr;
+  begin
+    write_byte(memory, address, byte, error_msg);
+    assert error_msg = null_string_ptr report to_string(error_msg);
+  end;
+
+  procedure read_byte(memory : memory_t; address : natural;
+                      variable byte : inout byte_t;
+                      variable error_msg : inout string_ptr_t) is
+  begin
+    check_address(memory, address, true, error_msg);
+    if error_msg /= null_string_ptr then
+      return;
+    end if;
+    byte := get(memory.p_data, address);
+  end;
+
+  impure function read_byte(memory : memory_t; address : natural) return byte_t is
+    variable byte : byte_t;
+    variable error_msg : string_ptr_t := null_string_ptr;
+  begin
+    read_byte(memory, address, byte, error_msg);
+    assert error_msg = null_string_ptr report to_string(error_msg);
+    return byte;
+  end;
+
+  impure function get_permissions(memory : memory_t; address : natural) return permissions_t is
+  begin
+    return permissions_t'val(get(memory.p_perm, address));
+  end;
+
+  procedure set_permissions(memory : memory_t; address : natural; permissions : permissions_t) is
+  begin
+    set(memory.p_perm, address, permissions_t'pos(permissions));
+  end procedure;
+
+  impure function describe_address(memory : memory_t; address : natural) return string is
+    variable alloc : alloc_t := address_to_allocation(memory, address);
+
+    impure function describe_allocation return string is
+    begin
+      if to_string(alloc.p_name) = "" then
+        return "anonymous allocation";
+      else
+        return "allocation '" & to_string(alloc.p_name) & "'";
+      end if;
+    end;
+  begin
+    if alloc = null_alloc then
+      return "address " & to_string(address) & " at unallocated location";
+    end if;
+
+    return ("address " & to_string(address) & " at offset " & to_string(address - base_address(alloc)) &
+            " within " & describe_allocation & " at range " &
+            "(" & to_string(base_address(alloc)) & " to " & to_string(last_address(alloc)) & ")");
+  end;
+
+end package body;
---
+++ vunit/vhdl/bfm/test/tb_memory.vhd
@@ -0,0 +1,155 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this file,
+-- You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Copyright (c) 2017, Lars Asplund lars.anders.asplund@gmail.com
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+
+use work.memory_pkg.all;
+use work.string_ptr_pkg.all;
+
+entity tb_memory is
+  generic (runner_cfg : string);
+end entity;
+
+architecture a of tb_memory is
+begin
+
+  main : process
+    variable memory : memory_t;
+    variable allocation : alloc_t;
+    variable error_msg : string_ptr_t;
+    variable byte : byte_t;
+  begin
+    test_runner_setup(runner, runner_cfg);
+
+    if run("Test default is null") then
+      assert memory = null_memory report "Default should be null memory";
+
+    elsif run("Test new memory") then
+      memory := new_memory;
+      assert memory /= null_memory report "Should not be null memory";
+
+    elsif run("Test allocate") then
+      memory := new_memory;
+
+      for i in 0 to 2 loop
+        allocation := allocate(memory, 1024);
+        check_equal(base_address(allocation), 1024*i, "base address");
+        check_equal(last_address(allocation), 1024*(i+1)-1, "last address");
+      end loop;
+
+    elsif run("Test allocate with alignment") then
+      memory := new_memory;
+      allocation := allocate(memory, 1);
+      allocation := allocate(memory, 1, alignment => 8);
+      check_equal(base_address(allocation), 8, "aligned base address");
+
+      assert get_permissions(memory, 1) = no_access report "No access at unallocated location";
+      assert get_permissions(memory, 7) = no_access report "No access at unallocated location";
+
+      allocation := allocate(memory, 1, alignment => 16);
+      check_equal(base_address(allocation), 16, "aligned base address");
+
+    elsif run("Test read and write byte") then
+      memory := new_memory;
+      allocation := allocate(memory, 2);
+
+      write_byte(memory, 0, 255);
+      write_byte(memory, 1, 128);
+      check_equal(read_byte(memory, 0), 255);
+      check_equal(read_byte(memory, 1), 128);
+
+    elsif run("Test access empty memory") then
+      memory := new_memory;
+      write_byte(memory, 0, 255, error_msg);
+      check_equal(to_string(error_msg), "Writing to empty memory");
+
+      read_byte(memory, 0, byte, error_msg);
+      check_equal(to_string(error_msg), "Reading from empty memory");
+
+    elsif run("Test access memory out of range") then
+      memory := new_memory;
+
+      allocation := allocate(memory, 1);
+
+      write_byte(memory, 1, 255, error_msg);
+      check_equal(to_string(error_msg), "Writing to memory out of range 0 to 0");
+
+      read_byte(memory, 1, byte, error_msg);
+      check_equal(to_string(error_msg), "Reading from memory out of range 0 to 0");
+
+    elsif run("Test default permissions") then
+      memory := new_memory;
+      allocation := allocate(memory, 1);
+      assert get_permissions(memory, 0) = read_and_write;
+
+    elsif run("Test set permissions") then
+      memory := new_memory;
+      allocation := allocate(memory, 1);
+
+      set_permissions(memory, 0, no_access);
+      assert get_permissions(memory, 0) = no_access;
+
+      set_permissions(memory, 0, write_only);
+      assert get_permissions(memory, 0) = write_only;
+
+      set_permissions(memory, 0, read_only);
+      assert get_permissions(memory, 0) = read_only;
+
+      set_permissions(memory, 0, read_and_write);
+      assert get_permissions(memory, 0) = read_and_write;
+
+    elsif run("Test access memory without permission (no_access)") then
+      memory := new_memory;
+      allocation := allocate(memory, 10);
+      set_permissions(memory, 5, no_access);
+
+      write_byte(memory, 5, 255, error_msg);
+      check_equal(to_string(error_msg), "Writing to " & describe_address(memory, 5) & " without permission (no_access)");
+
+      read_byte(memory, 5, byte, error_msg);
+      check_equal(to_string(error_msg), "Reading from " & describe_address(memory, 5) & " without permission (no_access)");
+
+    elsif run("Test access memory without permission (write_only)") then
+      memory := new_memory;
+      allocation := allocate(memory, 10);
+      set_permissions(memory, 5, write_only);
+
+      write_byte(memory, 5, 255, error_msg);
+      assert error_msg = null_string_ptr;
+
+      read_byte(memory, 5, byte, error_msg);
+      check_equal(to_string(error_msg), "Reading from " & describe_address(memory, 5) & " without permission (write_only)");
+
+    elsif run("Test access memory without permission (read_only)") then
+      memory := new_memory;
+      allocation := allocate(memory, 10);
+      set_permissions(memory, 5, read_only);
+
+      write_byte(memory, 5, 255, error_msg);
+      check_equal(to_string(error_msg), "Writing to " & describe_address(memory, 5) & " without permission (read_only)");
+
+      read_byte(memory, 5, byte, error_msg);
+      assert error_msg = null_string_ptr;
+
+    elsif run("Test describe address") then
+      memory := new_memory;
+      allocation := allocate(memory, 2);
+      allocation := allocate(memory, 10, name => "alloc_name");
+
+      check_equal(describe_address(memory, 12),
+                  "address 12 at unallocated location");
+      check_equal(describe_address(memory, 1),
+                  "address 1 at offset 1 within anonymous allocation at range (0 to 1)");
+      check_equal(describe_address(memory, 2),
+                  "address 2 at offset 0 within allocation 'alloc_name' at range (2 to 11)");
+      check_equal(describe_address(memory, 5),
+                  "address 5 at offset 3 within allocation 'alloc_name' at range (2 to 11)");
+    end if;
+
+    test_runner_cleanup(runner);
+  end process;
+end architecture;
