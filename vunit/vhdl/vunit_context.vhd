0d7655de1f8601518f6e2e9d4d462d6f33ba1dfe
Lars Asplund <lars.anders.asplund@gmail.com>
2017-11-09 23:08:15 +0100

    Updated vunit_context

--- vunit/vhdl/vunit_context.vhd
+++ vunit/vhdl/vunit_context.vhd
@@ -17,6 +17,7 @@ context vunit_context is
   use vunit_lib.ansi_pkg.all;
   use vunit_lib.checker_pkg.all;
   use vunit_lib.check_pkg.all;
+  use vunit_lib.check_deprecated_pkg.all;
   use vunit_lib.run_types_pkg.all;
   use vunit_lib.run_pkg.all;
 end context;
