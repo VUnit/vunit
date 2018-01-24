8a5c34af9c3a11ae93a9726bd1dd2779b81f25b1
Olof Kraigher <olof.kraigher@gmail.com>
2018-01-24 20:55:15 +0100

    Release 2.4.3

--- vunit/about.py
+++ vunit/about.py
@@ -62,7 +62,7 @@ def version():
     return '%i.%i.%i' % (VERSION[0], VERSION[1], VERSION[2])
 
 
-VERSION = (2, 4, 2)
+VERSION = (2, 4, 3)
 
 # DO NOT TOUCH: Only set to False by PyPI deployment script
 PRE_RELEASE = True
