1357617b83e8db78abde5e2eab410122a473d770
Lars Asplund <lars.anders.asplund@gmail.com>
2018-02-09 18:48:18 +0100

    Updated more license notices

--- examples/vhdl/run/tb_with_test_cases.vhd
+++ examples/vhdl/run/tb_with_test_cases.vhd
@@ -1,3 +1,9 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this file,
+-- You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Copyright (c) 2017-2018, Lars Asplund lars.anders.asplund@gmail.com
+
 library vunit_lib;
 context vunit_lib.vunit_context;
 
