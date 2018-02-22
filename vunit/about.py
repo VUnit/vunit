6bd0064974391749b4b912a384a304b3c0dbe44c
Lars Asplund <lars.asplund@autoliv.com>
2018-02-22 10:37:23 +0100

    Release 3.0.2

--- vunit/about.py
+++ vunit/about.py
@@ -62,7 +62,7 @@ def version():
     return '%i.%i.%i' % (VERSION[0], VERSION[1], VERSION[2])
 
 
-VERSION = (3, 0, 1)
+VERSION = (3, 0, 2)
 
 # DO NOT TOUCH: Only set to False by PyPI deployment script
 PRE_RELEASE = True
