1a04fe0f1497e0238777e9f1c1a79c76030fe859
Slawomir Siluk <slaweksiluk@gazeta.pl>
2018-02-27 10:30:17 +0100

    Pass simple (manual) tb for wishbone slave

--- vunit/vhdl/verification_components/test/tb_wishbone_slave.vhd
+++ vunit/vhdl/verification_components/test/tb_wishbone_slave.vhd
@@ -16,6 +16,7 @@ use ieee.numeric_std.all;
 
 library vunit_lib;
 context vunit_lib.vunit_context;
+context work.com_context;
 
 entity tb_wishbone_slave is
   generic (runner_cfg : string);
@@ -26,10 +27,10 @@ architecture a of tb_wishbone_slave is
   constant adr_width    : positive := 4;
 
   signal clk    : std_logic := '0';
-  signal adr    : std_logic_vector(adr_width-1 downto 0);
-  signal dat_i  : std_logic_vector(dat_width-1 downto 0);
-  signal dat_o  : std_logic_vector(dat_width-1 downto 0);
-  signal sel   : std_logic_vector(dat_width/8 -1 downto 0);
+  signal adr    : std_logic_vector(adr_width-1 downto 0) := (others => '0');
+  signal dat_i  : std_logic_vector(dat_width-1 downto 0) := (others => '0');
+  signal dat_o  : std_logic_vector(dat_width-1 downto 0) := (others => '0');
+  signal sel   : std_logic_vector(dat_width/8 -1 downto 0) := (others => '1');
   signal cyc   : std_logic := '0';
   signal stb   : std_logic := '0';
   signal we    : std_logic := '0';
@@ -48,119 +49,56 @@ begin
     test_runner_setup(runner, runner_cfg);
 	  wait until rising_edge(clk);
 
-    if run("wr single rd single") then
-      write_bus(net, bus_handle, 0, value);
-      
-      wait until ack = '1' and rising_edge(clk);
-      wait until rising_edge(clk);
-      read_bus(net, bus_handle, 0, tmp);
-      check_equal(tmp, value, "read data");
-      
---    elsif run("wr block") then
---      -- TODO not sure if is allowed to toggle signal we during
---      -- wishbone single cycle
---      write_bus(net, bus_handle, 0, value);
---      read_bus(net, bus_handle, 0, tmp);
---      check_equal(tmp, value, "read data");
-      
-    elsif run("wr block rd single") then
+
+    if run("wr block rd block") then
       info(tb_logger, "Writing...");
       for i in 0 to num_block_cycles-1 loop
-        write_bus(net, bus_handle, i,
-            std_logic_vector(to_unsigned(i, dat_i'length)));
+				cyc <= '1';
+				stb <= '1';
+				we	<= '1';
+				adr <= std_logic_vector(to_unsigned(i, adr'length));
+				dat_i <= std_logic_vector(to_unsigned(i, dat_i'length));
+				wait until rising_edge(clk);
       end loop;
-      
-      for i in 1 to num_block_cycles loop
-        wait until ack = '1' and rising_edge(clk);
-      end loop;
-      wait until rising_edge(clk);
-
-      info(tb_logger, "Reading...");
-      for i in 0 to num_block_cycles-1 loop
-        read_bus(net, bus_handle, i, tmp);
-        check_equal(tmp, std_logic_vector(to_unsigned(i, dat_i'length)), "read data");
-      end loop;
-
-    elsif run("wr block rd block") then
-      info(tb_logger, "Writing...");
-      for i in 0 to num_block_cycles-1 loop
-        write_bus(net, bus_handle, i,
-            std_logic_vector(to_unsigned(i, dat_i'length)));
-      end loop;
+			cyc <= '0';
+			stb <= '0';
+			wait until rising_edge(clk);      
       
       info(tb_logger, "Sleeping...");
-      for i in 1 to num_block_cycles loop
-        wait until ack = '1' and rising_edge(clk);
-      end loop;
       wait for 100 ns;
       wait until rising_edge(clk);
 
       info(tb_logger, "Reading...");
       for i in 0 to num_block_cycles-1 loop
-        read_bus(net, bus_handle, i, rd_ref(i));
+				cyc <= '1';
+				stb <= '1';
+				we	<= '0';
+				adr <= std_logic_vector(to_unsigned(i, adr'length));
+				--info(tb_logger, "dat_o="&to_hstring(dat_o));
+				--check_equal(dat_o, std_logic_vector(to_unsigned(i, dat_o'length)), "dat_o");
+				wait until rising_edge(clk);
       end loop;
-
-      wait until rising_edge(clk);
-      wait until rising_edge(clk);
-      wait until rising_edge(clk);
-      wait for 100 ns;
-	
-      trace(tb_logger, "Get reads by references...");
-      for i in 0 to num_block_cycles-1 loop
-        await_read_bus_reply(net, rd_ref(i), tmp);
-        --check_equal(tmp, std_logic_vector(to_unsigned(i, dat_i'length)), "read data");
-      end loop;
-	  -- With references
---    elsif run("WR block") then
---      for i in 0 to num_block_cycles-1 loop
---        write_bus(net, bus_handle, i,
---            std_logic_vector(to_unsigned(i, dat_i'length)));
---      end loop;
-
---      trace(tb_logger, "Test block read");
---      for i in 0 to num_block_cycles-1 loop
---        read_bus(net, bus_handle, i, rd_ref(i));
---      end loop;
---      trace(tb_logger, "Get data by its references");
---      for i in 0 to num_block_cycles-1 loop
---        await_read_bus_reply(net, rd_ref(i), tmp);
---        check_equal(tmp, std_logic_vector(to_unsigned(i, dat_i'length)), "read data");
---      end loop;
+			cyc <= '0';
+			stb <= '0';
     end if;
 
-    wait for 1000 ns;
+    wait for 200 ns;
     test_runner_cleanup(runner);
     wait;
   end process;
-  test_runner_watchdog(runner, 100 us);
+  test_runner_watchdog(runner, 1 us);
   set_format(display_handler, verbose, true);
   show(tb_logger, display_handler, verbose);
   show(default_logger, display_handler, verbose);
-  show(master_logger, display_handler, verbose);
   show(com_logger, display_handler, verbose);
   
-
-  dut : entity work.wishbone_master
-    generic map (
-      bus_handle => bus_handle)
-    port map (
-      clk   => clk,
-      adr   => adr,
-      dat_i => dat_i,
-      dat_o => dat_o,
-      sel   => sel,
-      cyc   => cyc,
-      stb   => stb,
-      we    => we,
-      ack   => ack
-    );
     
   dut_slave : entity work.wishbone_slave
     port map (
       clk   => clk,
       adr   => adr,
-      dat_i => dat_o,
-      dat_o => dat_i,
+      dat_i => dat_i,
+      dat_o => dat_o,
       sel   => sel,
       cyc   => cyc,
       stb   => stb,
