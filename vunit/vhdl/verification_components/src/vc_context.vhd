f889ecb6a693d85d02e4be383c024d4f68a20fc5
Lars Asplund <lars.anders.asplund@gmail.com>
2018-04-26 22:40:13 +0200

    Extended vc_context.vhd with missing packages.

--- vunit/vhdl/verification_components/src/vc_context.vhd
+++ vunit/vhdl/verification_components/src/vc_context.vhd
@@ -14,5 +14,7 @@ context vc_context is
   use vunit_lib.uart_pkg.all;
   use vunit_lib.stream_master_pkg.all;
   use vunit_lib.stream_slave_pkg.all;
+  use vunit_lib.memory_utils_pkg.all;
+  use vunit_lib.sync_pkg.all;
   context vunit_lib.com_context;
 end context;
