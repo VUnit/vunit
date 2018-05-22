4be4d4365d97342aebce4439d16b878ac9a4b717
1138-4EB <1138-4EB@users.noreply.github.com>
2018-05-22 02:14:14 +0200

    add decode function example (VHDL)

--- examples/vhdl/json4vhdl/src/test/tb_json_gens.vhd
+++ examples/vhdl/json4vhdl/src/test/tb_json_gens.vhd
@@ -17,8 +17,23 @@ entity tb_json_gens is
 end entity;
 
 architecture tb of tb_json_gens is
+
   constant JSONContent     : T_JSON := jsonLoad(tb_cfg);
   constant JSONFileContent : T_JSON := jsonLoad(tb_path & tb_cfg_file);
+
+  type tb_img_t is record
+    image_width     : positive;
+    image_height    : positive;
+  end record tb_img_t;
+
+  impure function decode(Content : T_JSON) return tb_img_t is
+  begin
+    return (image_width => positive'value( jsonGetString(Content, "Image/0") ),
+            image_height => positive'value( jsonGetString(Content, "Image/1") ));
+  end function decode;
+
+  constant tb_img : tb_img_t := decode(JSONContent);
+
 begin
   main: process
   begin
@@ -26,14 +41,17 @@ begin
     while test_suite loop
       if run("test") then
         info("JSONContent: " & lf & JSONContent.Content);
-        info("Image: " & jsonGetString(JSONContent, "Image/0") & ',' & jsonGetString(JSONContent, "Image/1"));
-        info("Platform/ML505/FPGA: " & jsonGetString(JSONContent, "Platform/ML505/FPGA"));
-        info("Platform/KC705/IIC/0/Devices/0/Name: " & jsonGetString(JSONContent, "Platform/KC705/IIC/0/Devices/0/Name"));
 
         info("tb_path & tb_cfg_file: " & tb_path & tb_cfg_file);
         info("JSONFileContent: " & lf & JSONFileContent.Content);
+
+        info("Image: " & integer'image(tb_img.image_width) & ',' & integer'image(tb_img.image_height));
         info("Image: " & jsonGetString(JSONFileContent, "Image/0") & ',' & jsonGetString(JSONFileContent, "Image/1"));
+
+        info("Platform/ML505/FPGA: " & jsonGetString(JSONContent, "Platform/ML505/FPGA"));
         info("Platform/ML505/FPGA: " & jsonGetString(JSONFileContent, "Platform/ML505/FPGA"));
+
+        info("Platform/KC705/IIC/0/Devices/0/Name: " & jsonGetString(JSONContent, "Platform/KC705/IIC/0/Devices/0/Name"));
         info("Platform/KC705/IIC/0/Devices/0/Name: " & jsonGetString(JSONFileContent, "Platform/KC705/IIC/0/Devices/0/Name"));
       end if;
     end loop;
