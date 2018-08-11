c39bde66649d123265472163bc6e29aaf0c7434c
Emanuel Schmid <emanuel.schmid@scs.ch>
2018-08-11 23:38:46 +0200

    wishbone_master: strictly keep order of commands

--- vunit/vhdl/verification_components/src/wishbone_master.vhd
+++ vunit/vhdl/verification_components/src/wishbone_master.vhd
@@ -15,6 +15,7 @@ context work.com_context;
 use work.com_types_pkg.all;
 use work.logger_pkg.all;
 use work.check_pkg.all;
+use work.sync_pkg.all;
 
 library osvvm;
 use osvvm.RandomPkg.all;
@@ -44,94 +45,93 @@ architecture a of wishbone_master is
   constant acknowledge_queue : queue_t := new_queue;
   constant bus_ack_msg   : msg_type_t := new_msg_type("wb master ack msg");
   constant wb_master_ack_actor : actor_t := new_actor;
+  signal start_cycle : std_logic := '0';
+  signal end_cycle : std_logic := '0';
+  signal cycle : boolean;
 begin
 
   main : process
     variable request_msg : msg_t;
     variable msg_type : msg_type_t;
-  begin
-      request_msg := null_msg;
-      receive(net, bus_handle.p_actor, request_msg);
-      msg_type := message_type(request_msg);
-      if msg_type = bus_read_msg then
-        push(rd_request_queue, request_msg);
-        request_msg := null_msg;
-      elsif msg_type = bus_write_msg then
-        push(wr_request_queue, request_msg);
-        request_msg := null_msg;
-      else
-        unexpected_msg_type(msg_type);
-      end if;
-  end process;
-
-  request : process
-    variable request_msg : msg_t;
-    variable ack_msg : msg_t;
-    variable pending_acks : natural := 0;
-    variable received_acks : natural := 0;
-    variable rd_cycle : boolean;
-    variable wr_cycle : boolean;
+    variable cycle_type : msg_type_t;
     variable rnd : RandomPType;
   begin
     rnd.InitSeed(rnd'instance_name);
     report rnd'instance_name;
-    cyc <= '0';
+    request_msg := null_msg;
+    cycle_type := bus_read_msg;
     stb <= '0';
+
     wait until rising_edge(clk);
     loop
-      check_false(rd_cycle and wr_cycle);
-      if not is_empty(rd_request_queue) and not wr_cycle then
-        request_msg := pop(rd_request_queue);
-        rd_cycle := true;
-        cyc <= '1';
-        while rnd.Uniform(0.0, 1.0) > strobe_high_probability loop
-          wait until rising_edge(clk);
-        end loop;
-        adr <= pop_std_ulogic_vector(request_msg);
-        -- TODO why sel is not passed in msg for reading (present for writing)?
-        --sel <= pop_std_ulogic_vector(request_msg);
-        stb <= '1';
-        we <= '0';
-        wait until rising_edge(clk) and stall = '0';
-        stb <= '0';
-        push(acknowledge_queue, request_msg);
-        pending_acks := pending_acks +1;
+      receive(net, bus_handle.p_actor, request_msg);
+      msg_type := message_type(request_msg);
 
-      elsif not is_empty(wr_request_queue) and not rd_cycle then
-        request_msg := pop(wr_request_queue);
-        wr_cycle := true;
-        cyc <= '1';
-        while rnd.Uniform(0.0, 1.0) > strobe_high_probability loop
-          wait until rising_edge(clk);
-        end loop;
-        adr <= pop_std_ulogic_vector(request_msg);
-        dat_o <= pop_std_ulogic_vector(request_msg);
-        sel <= pop_std_ulogic_vector(request_msg);
-        stb <= '1';
-        we <= '1';
-        wait until rising_edge(clk) and stall = '0';
-        stb <= '0';
-        push(acknowledge_queue, request_msg);
-        pending_acks := pending_acks +1;
-
-      -- During cylce but no msg from tb? Have to wait for
-      -- one - block on receive
-      elsif (wr_cycle or rd_cycle) then
-        receive(net, wb_master_ack_actor, ack_msg);
-        received_acks := received_acks +1;
-        if pending_acks = received_acks then
-          -- End of wb cycle
-          cyc <= '0';
-          pending_acks := 0;
-          received_acks := 0;
-          rd_cycle := false;
-          wr_cycle := false;
+      if msg_type = bus_read_msg or msg_type = bus_write_msg then
+        if msg_type /= cycle_type and cycle then
+          wait until not cycle; -- todo: is this necessary? the wb spec v4 does not explicitly forbid mixed cycles
           wait until rising_edge(clk);
         end if;
 
-      -- No cycles, wait one clk period
+        push(acknowledge_queue, request_msg);
+        start_cycle <= not start_cycle;
+        cycle_type := msg_type;
+
+        while rnd.Uniform(0.0, 1.0) > strobe_high_probability loop
+          wait until rising_edge(clk);
+        end loop;
+        adr <= pop_std_ulogic_vector(request_msg);
+        stb <= '1';
+        if msg_type = bus_write_msg then
+          we <= '1';
+          dat_o <= pop_std_ulogic_vector(request_msg);
+          sel <= pop_std_ulogic_vector(request_msg);
+        else
+          we <= '0';
+          -- TODO why sel is not passed in msg for reading (present for writing)?
+          --sel <= pop_std_ulogic_vector(request_msg);
+        end if;
+        wait until rising_edge(clk) and stall = '0';
+        stb <= '0';
+
+        request_msg := null_msg;
+
+      elsif msg_type = wait_until_idle_msg then
+        handle_wait_until_idle(net, msg_type, request_msg);
+
       else
-        wait until rising_edge(clk);
+        unexpected_msg_type(msg_type);
+
+      end if;
+    end loop;
+  end process;
+
+  p_cycle : process
+    variable request_msg : msg_t;
+    variable ack_msg : msg_t;
+    variable pending : natural := 0;
+    variable received_acks : natural := 0;
+  begin
+    cyc <= '0';
+    cycle <= false;
+
+    loop
+      wait until start_cycle'event or end_cycle'event;
+
+      if start_cycle'event then
+        pending := pending+1;
+      end if;
+      if end_cycle'event then
+        pending := pending-1;
+      end if;
+      assert pending >= 0 report "Pending transactions became negative" severity failure;
+
+      if pending > 0 then
+        cyc <= '1';
+        cycle <= true;
+      else
+        cyc <= '0';
+        cycle <= false;
       end if;
     end loop;
   end process;
@@ -149,7 +149,6 @@ begin
     end if;
     delete(request_msg);
     -- Response main sequencer that ack is received
-    ack_msg := new_msg(bus_ack_msg);
-    send(net, wb_master_ack_actor, ack_msg);
+    end_cycle <= not end_cycle;
   end process;
 end architecture;
