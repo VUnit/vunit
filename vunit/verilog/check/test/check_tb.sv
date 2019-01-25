64436f1f096fb30f3882f77a867bcdf7b3f7ffc0
dbalthazor <dbalthazor@gmail.com>
2019-01-25 15:53:09 -0800

    Add missing copyright notice on check_tb.sv

--- vunit/verilog/check/test/check_tb.sv
+++ vunit/verilog/check/test/check_tb.sv
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2015-2016, Lars Asplund lars.anders.asplund@gmail.com
+
 `timescale 10ns / 10ns
 `include "vunit_defines.svh"
 
