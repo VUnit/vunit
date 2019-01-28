532887afa48ece9f354b46599392da48e1e22db2
dbalthazor <dbalthazor@gmail.com>
2019-01-28 09:32:27 -0800

    Fixed some linting errors

--- vunit/verilog/check/run.py
+++ vunit/verilog/check/run.py
@@ -16,23 +16,6 @@ lib = vu.add_library("lib")
 
 tb_list = lib.add_source_files(join(root, "test", "*.sv"))
 
-
 vu.set_sim_option("modelsim.vsim_flags.gui", ["-novopt"])
 
-#Uncomment these later when coverage reporting is added
-#vu.set_sim_option("modelsim.vsim_flags", ["-assertcover"])
-
-#lib.set_compile_option("modelsim.vcom_flags", ["+cover=bs"])
-#lib.set_compile_option("modelsim.vlog_flags", ["+cover=bs"])
-#lib.set_sim_option("enable_coverage", True)
-
-def post_run(results):
-    results.merge_coverage(
-        file_name=join(root, "vunit_out", "mlc_coverage_data.ucdb"),
-        args=["-quiet"]
-    )
-
-
 vu.main()
-#TODO: Compile coverage report
-#vu.main(post_run=post_run)
