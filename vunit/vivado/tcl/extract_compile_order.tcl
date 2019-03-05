a26189f38b7a5b26e5b4154b3097ae4800dc9e45
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2019-03-05 15:59:06 +0100

    Avoid compiling IP cores within the block design

--- vunit/vivado/tcl/extract_compile_order.tcl
+++ vunit/vivado/tcl/extract_compile_order.tcl
@@ -5,11 +5,13 @@ open_project ${project_file}
 
 set file_out [open ${output_file} w]
 foreach ip [get_ips] {
-    generate_target {simulation synthesis} ${ip}
-    foreach src_file [get_files -used_in simulation -compile_order sources -of_objects ${ip}] {
-        set library [get_property LIBRARY ${src_file}]
-        set file_type [get_property FILE_TYPE ${src_file}]
-        puts ${file_out} "${library},${file_type},${src_file}"
+    if {[get_property SCOPE ${ip}] eq ""} {
+        generate_target {simulation synthesis} ${ip}
+        foreach src_file [get_files -used_in simulation -compile_order sources -of_objects ${ip}] {
+            set library [get_property LIBRARY ${src_file}]
+            set file_type [get_property FILE_TYPE ${src_file}]
+            puts ${file_out} "${library},${file_type},${src_file}"
+        }
     }
 }
 close ${file_out}
