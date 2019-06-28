f84902c406fba31636ad6602c27ff6ba5dd962d0
Lars Asplund <lars.anders.asplund@gmail.com>
2019-06-28 21:51:58 +0200

    Fixed broken tests

--- examples/vhdl/run/tb_with_watchdog.vhd
+++ examples/vhdl/run/tb_with_watchdog.vhd
@@ -21,16 +21,16 @@ begin
     while test_suite loop
       if run("Test that stalls") then
         wait;
-        
+
       elsif run("Test to_string for boolean") then
         check_equal(to_string(true), "true");
-        
+
       elsif run("Test that needs longer timeout") then
         -- It is also possible to set/re-set the timeout
         -- When test cases need separate timeout settings
         set_timeout(runner, 2 ms);
         wait for 1 ms;
-        
+
       elsif run("Test that stalling processes can inform why they caused a timeout") then
         wait until (and foo);
       end if;
