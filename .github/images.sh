0a8b199839f047399690739499dc91685fc3e722
1138-4EB <1138-4EB@users.noreply.github.com>
2019-09-09 07:38:45 +0200

    ci: add GHA workflow 'images.yml'

---
+++ .github/images.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Stop in case of error
+set -e
+
+docker build \
+  --build-arg IMAGE="python:$PYVER-slim-buster" \
+  --build-arg LLVM_VER=7 \
+  --build-arg GNAT_VER=8 \
+  --target vunit \
+  -t "vunit/dev:${TAG}" \
+  - <<-EOF
+$(curl -fsSL https://raw.githubusercontent.com/ghdl/docker/master/dockerfiles/run_debian)
+
+FROM $TGT AS vunit
+COPY --from=ghdl/pkg:buster-$PKG / /test
+
+RUN tar xzf \$(ls /test/ghdl-*) -C /usr/local/ \\
+ && rm -rf /test \
+ && pip install tox colorama
+EOF
