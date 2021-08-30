fbd3c7cbb658b52f4fee71de67fcd8aa05087ec9
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2021-08-30 18:39:11 -0300

    Fix compile issues under vunit/vhdl/data_types/src

--- vunit/vhdl/data_types/src/queue_pkg.vhd
+++ vunit/vhdl/data_types/src/queue_pkg.vhd
@@ -344,6 +344,8 @@ package queue_pkg is
     code : string
   ) return queue_t;
 
+  alias decode_queue_t is decode[string return queue_t];
+
   procedure decode (
     constant code   : string;
     variable index  : inout positive;
@@ -351,7 +353,6 @@ package queue_pkg is
   );
 
   alias encode_queue_t is encode[queue_t return string];
-  alias decode_queue_t is decode[string return queue_t];
 
   procedure push_type (
     queue        : queue_t;
