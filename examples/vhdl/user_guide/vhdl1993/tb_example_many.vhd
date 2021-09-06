a07a861a2e84579384c11171512433f29109c6a0
umarcor <unai.martinezcorral@ehu.eus>
2021-09-06 11:48:35 +0200

    examples/vhdl/user_guide: add VHDL 1993 variant

---
+++ examples/vhdl/user_guide/vhdl1993/tb_example_many.vhd
@@ -0,0 +1,59 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this file,
+-- You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Copyright (c) 2014-2021, Lars Asplund lars.anders.asplund@gmail.com
+
+library vunit_lib;
+
+use vunit_lib.types_pkg.all;
+use vunit_lib.integer_vector_ptr_pkg.all;
+use vunit_lib.integer_vector_ptr_pool_pkg.all;
+use vunit_lib.integer_array_pkg.all;
+use vunit_lib.queue_pkg.all;
+use vunit_lib.queue_pool_pkg.all;
+use vunit_lib.string_ptr_pkg.all;
+use vunit_lib.string_ptr_pool_pkg.all;
+use vunit_lib.byte_vector_ptr_pkg.all;
+use vunit_lib.dict_pkg.all;
+
+use vunit_lib.string_ops.all;
+use vunit_lib.dictionary.all;
+use vunit_lib.path.all;
+use vunit_lib.print_pkg.all;
+use vunit_lib.log_levels_pkg.all;
+use vunit_lib.logger_pkg.all;
+use vunit_lib.log_handler_pkg.all;
+use vunit_lib.log_deprecated_pkg.all;
+use vunit_lib.ansi_pkg.all;
+use vunit_lib.checker_pkg.all;
+use vunit_lib.check_pkg.all;
+use vunit_lib.check_deprecated_pkg.all;
+use vunit_lib.run_types_pkg.all;
+use vunit_lib.run_pkg.all;
+use vunit_lib.run_deprecated_pkg.all;
+
+entity tb_example_many is
+  generic (runner_cfg : string);
+end entity;
+
+architecture tb of tb_example_many is
+begin
+  main : process
+  begin
+    test_runner_setup(runner, runner_cfg);
+
+    while test_suite loop
+
+      if run("test_pass") then
+        report "This will pass";
+
+      elsif run("test_fail") then
+        assert false report "It fails";
+
+      end if;
+    end loop;
+
+    test_runner_cleanup(runner);
+  end process;
+end architecture;
