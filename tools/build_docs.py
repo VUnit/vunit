deda28b881bb29afd1a9279fbb80b3b931a4908d
Unai Martinez-Corral <38422348+umarcor@users.noreply.github.com>
2021-10-08 16:12:46 +0100

    docs: use 'exec' directive to generate content in examples.rst (#739)

--- tools/build_docs.py
+++ tools/build_docs.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -14,10 +16,24 @@ import sys
 from sys import argv
 from shutil import copyfile
 from create_release_notes import create_release_notes
-from docs_utils import examples, get_theme
 
 
 DROOT = Path(__file__).parent.parent / 'docs'
+
+
+def get_theme(path: Path, url: str):
+    """
+    Check if the theme is available locally, retrieve it with curl and tar otherwise
+    """
+    tpath = path / "_theme"
+    if not tpath.is_dir() or not (tpath / "theme.conf").is_file():
+        if not tpath.is_dir():
+            tpath.mkdir()
+        zpath = path / "theme.tgz"
+        if not zpath.is_file():
+            check_call(["curl", "-fsSL", url, "-o", str(zpath)])
+        tar_cmd = ["tar", "--strip-components=1", "-C", str(tpath), "-xvzf", str(zpath)]
+        check_call(tar_cmd)
 
 
 def main():
@@ -25,7 +41,6 @@ def main():
     Build documentation/website
     """
     create_release_notes()
-    examples()
     copyfile(str(DROOT / '..' / 'LICENSE.rst'), str(DROOT / 'license.rst'))
     get_theme(
         DROOT,
