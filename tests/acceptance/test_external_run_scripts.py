35bf6be10dec2ddb82b39d80902c34cc7697f3d4
umarcor <unai.martinezcorral@ehu.eus>
2021-10-20 05:26:13 +0200

    remove 'array_pkg'

--- tests/acceptance/test_external_run_scripts.py
+++ tests/acceptance/test_external_run_scripts.py
@@ -228,9 +228,6 @@ class TestExternalRunScripts(TestCase):
     def test_vhdl_com_example_project(self):
         self.check(ROOT / "examples/vhdl/com/run.py")
 
-    def test_array_vhdl_2008(self):
-        self.check(VHDL_PATH / "array/run.py")
-
     def test_data_types_vhdl_2008(self):
         self.check(VHDL_PATH / "data_types/run.py")
 
