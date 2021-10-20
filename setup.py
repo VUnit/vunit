85765a4aba2489f9f7cb85f0b7b334c7435acf49
umarcor <unai.martinezcorral@ehu.eus>
2021-10-20 04:38:58 +0200

    setup: add Python 3.10 to classifiers

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
