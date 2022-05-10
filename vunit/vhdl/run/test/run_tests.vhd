cf1ab91e6faabea3a7440553e56aad8177f38fcc
std-max <85441682+std-max@users.noreply.github.com>
2022-05-10 12:58:44 +0200

    Raise an error when a test case is called twice

--- vunit/vhdl/run/test/run_tests.vhd
+++ vunit/vhdl/run/test/run_tests.vhd
@@ -471,15 +471,17 @@ begin
     banner("Should prevent from running a test from enabled_test_case more than once unless re-initialized.");
     test_case_setup;
     test_runner_setup(runner, "enabled_test_cases : Should one");
-    for i in 0 to 1 loop
+    for i in 0 to 2 loop
       case i is
         when 0 =>
           check(c, run("Should one"), "Expected ""Should one"" to run.");
         when others =>
+          mock(runner_trace_logger);
           check_false(c, run("Should one"), "Didn't expected ""Should one"" to run.");
+          check_log(runner_trace_logger, "Test case: Should one cannot be run more than once unless re-initialization.", error);
+          unmock(runner_trace_logger);
       end case;
     end loop;
-    check_false(c, run("Should one"), "Didn't expect ""Should one"" to run.");
     test_runner_setup(runner, "enabled_test_cases : Should one");
     while test_suite loop
       check(c, run("Should one"), "Expected ""Should one"" to run.");
