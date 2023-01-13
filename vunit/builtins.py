d408623765190fe3ab806650876de601f3da9854
Lars Asplund <lars.anders.asplund@gmail.com>
2023-01-14 00:15:24 +0100

    Move event packages to data_types

--- vunit/builtins.py
+++ vunit/builtins.py
@@ -278,7 +278,6 @@ in your VUnit Git repository? You have to do this first if installing using setu
             "dictionary",
             "run",
             "path",
-            "event",
         ):
             self._add_files(VHDL_PATH / path / "src" / "*.vhd")
 
