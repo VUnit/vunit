6df31e69893a2a7d4d1108e0288f5a4e1864a77e
Martin Cizmar (LavovaLampa) <betacheer@gmail.com>
2023-05-27 18:35:41 +0200

    Add type hints to "parsing/"

--- vunit/parsing/verilog/tokenizer.py
+++ vunit/parsing/verilog/tokenizer.py
@@ -12,120 +12,101 @@
 Verilog preprocessing
 """
 
-from vunit.parsing.tokenizer import Tokenizer, Token
-from vunit.parsing.verilog.tokens import (
-    COLON,
-    COMMA,
-    COMMENT,
-    EQUAL,
-    ESCAPED_NEWLINE,
-    DOUBLE_COLON,
-    HASH,
-    IDENTIFIER,
-    KEYWORDS,
-    LBRACE,
-    LBRACKET,
-    LPAR,
-    MULTI_COMMENT,
-    NEWLINE,
-    OTHER,
-    PREPROCESSOR,
-    RBRACE,
-    RBRACKET,
-    RPAR,
-    SEMI_COLON,
-    STRING,
-    WHITESPACE,
-)
+from typing import Callable, Optional
+from vunit.parsing.tokenizer import Location, Tokenizer, Token
+from vunit.parsing.verilog.tokens import KEYWORDS, KeywordKind, TokenKind
 
 
-class VerilogTokenizer(object):
+class VerilogTokenizer:
     """
     A Verilog tokenizer
     """
 
-    def __init__(self, create_locations=True):
+    _tokenizer: Tokenizer
+    _create_locations: bool
+
+    def __init__(self, create_locations: bool = True):
         self._tokenizer = Tokenizer()
         self._create_locations = create_locations
 
-        def slice_value(token, start=None, end=None):
+        def slice_value(token: Token, start: Optional[int] = None, end: Optional[int] = None):
             return Token(token.kind, token.value[start:end], token.location)
 
-        def str_value(token):
+        def str_value(token: Token) -> Token:
             return Token(
                 token.kind,
                 token.value[1:-1].replace("\\\n", "").replace('\\"', '"'),
                 token.location,
             )
 
-        def remove_value(token):
+        def remove_value(token: Token) -> Token:
             return Token(token.kind, "", token.location)
 
-        def ignore_value(token):  # pylint: disable=unused-argument
+        def ignore_value(_: Token) -> None:  # pylint: disable=unused-argument
             pass
 
-        def add(kind, regex, func=None):
+        def add(kind: TokenKind, regex: str, func: Optional[Callable[[Token], Optional[Token]]] = None):
             self._tokenizer.add(kind, regex, func)
 
-        def replace_keywords(token):  # pylint: disable=missing-docstring
+        def replace_keywords(token: Token) -> Token:  # pylint: disable=missing-docstring
             if token.value in KEYWORDS:
-                return Token(KEYWORDS[token.value], token.value, token.location)
+                return Token(KeywordKind(token.value), token.value, token.location)
 
             return token
 
         add(
-            PREPROCESSOR,
+            TokenKind.PREPROCESSOR,
             r"`[a-zA-Z][a-zA-Z0-9_]*",
             lambda token: slice_value(token, start=1),
         )
 
-        add(STRING, r'(?<!\\)"((.|\n)*?)(?<!\\)"', str_value)
+        add(TokenKind.STRING, r'(?<!\\)"((.|\n)*?)(?<!\\)"', str_value)
 
-        add(COMMENT, r"//.*$", lambda token: slice_value(token, start=2))
+        add(TokenKind.COMMENT, r"//.*$", lambda token: slice_value(token, start=2))
 
-        add(IDENTIFIER, r"[a-zA-Z_][a-zA-Z0-9_]*", replace_keywords)
+        add(TokenKind.IDENTIFIER, r"[a-zA-Z_][a-zA-Z0-9_]*", replace_keywords)
 
-        add(ESCAPED_NEWLINE, r"\\\n", ignore_value)
+        add(TokenKind.ESCAPED_NEWLINE, r"\\\n", ignore_value)
 
-        add(NEWLINE, r"\n", remove_value)
+        add(TokenKind.NEWLINE, r"\n", remove_value)
 
-        add(WHITESPACE, r"[ \t]+")
+        add(TokenKind.WHITESPACE, r"[ \t]+")
 
         add(
-            MULTI_COMMENT,
+            TokenKind.MULTI_COMMENT,
             r"/\*(.|\n)*?\*/",
             lambda token: slice_value(token, start=2, end=-2),
         )
 
-        add(DOUBLE_COLON, r"::", remove_value)
+        add(TokenKind.DOUBLE_COLON, r"::", remove_value)
 
-        add(COLON, r":", remove_value)
+        add(TokenKind.COLON, r":", remove_value)
 
-        add(SEMI_COLON, r";", remove_value)
+        add(TokenKind.SEMI_COLON, r";", remove_value)
 
-        add(HASH, r"\#", remove_value)
+        add(TokenKind.HASH, r"\#", remove_value)
 
-        add(EQUAL, r"=", remove_value)
+        add(TokenKind.EQUAL, r"=", remove_value)
 
-        add(LPAR, r"\(", remove_value)
+        add(TokenKind.LPAR, r"\(", remove_value)
 
-        add(RPAR, r"\)", remove_value)
+        add(TokenKind.RPAR, r"\)", remove_value)
 
-        add(LBRACKET, r"\[", remove_value)
+        add(TokenKind.LBRACKET, r"\[", remove_value)
 
-        add(RBRACKET, r"\]", remove_value)
+        add(TokenKind.RBRACKET, r"\]", remove_value)
 
-        add(LBRACE, r"{", remove_value)
+        add(TokenKind.LBRACE, r"{", remove_value)
 
-        add(RBRACE, r"}", remove_value)
+        add(TokenKind.RBRACE, r"}", remove_value)
 
-        add(COMMA, r",", remove_value)
+        add(TokenKind.COMMA, r",", remove_value)
 
-        add(OTHER, r".+?")
+        add(TokenKind.OTHER, r".+?")
 
         self._tokenizer.finalize()
 
-    def tokenize(self, code, file_name=None, previous_location=None):
+    def tokenize(self, code: str, file_name: Optional[str] = None, previous_location: Optional[Location] = None):
         """
         Tokenize Verilog code to be preprocessed
         """
