ea3132779b6746e658c584e5e2d334bbe4ddd219
Piotr Rzeszut <piotrrze@gmail.com>
2023-10-05 16:03:32 +0200

    Move case-sensitivity check to get_test_bench (see #945)

--- vunit/ui/library.py
+++ vunit/ui/library.py
@@ -486,12 +486,7 @@ class Library(object):
         :returns: A :class:`.TestBench` object
         :raises: KeyError
         """
-        name_lower = name.lower()
-        
-        try:
-            return TestBench(self._test_bench_list.get_test_bench(self._library_name, name_lower), self)
-        except:
-            return TestBench(self._test_bench_list.get_test_bench(self._library_name, name), self)
+        return TestBench(self._test_bench_list.get_test_bench(self._library_name, name), self)
 
     def get_test_benches(self, pattern="*", allow_empty=False):
         """
