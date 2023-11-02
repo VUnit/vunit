58749fb14c00b0326891d2ac4b3f5775ad10533c
Szymon <147648237+SzymonHitachi@users.noreply.github.com>
2023-11-02 15:50:18 +0100

    Update extract_compile_order.tcl
    Add support for out of scope BD build. Per @jh16g15

--- vunit/vivado/tcl/extract_compile_order.tcl
+++ vunit/vivado/tcl/extract_compile_order.tcl
@@ -28,13 +28,21 @@ foreach ip [get_ips] {
 }
 
 # Build compilation order for all block designs
+set current_simset [current_fileset -simset]
+set current_sim_top [get_property top [get_filesets $current_simset]]
+set current_sim_top_lib [get_property top_lib [get_filesets $current_simset]]
 # -quiet is required otherwise get_bd_designs will error if there are no block designs
 foreach bd [get_bd_designs -quiet] {
+    set_property top ${bd} [get_filesets $current_simset];
+    update_compile_order -fileset $current_simset
     foreach src_file [get_files -used_in simulation -compile_order sources *${bd}*] {
         set library [get_property LIBRARY ${src_file}]
         set file_type [get_property FILE_TYPE ${src_file}]
         puts ${file_out} "${library},${file_type},${src_file}"
     }
 }
+set_property top ${current_sim_top} [get_filesets $current_simset]
+set_property top_lib ${current_sim_top_lib} [get_filesets $current_simset]
+update_compile_order -fileset $current_simset
 
 close ${file_out}
