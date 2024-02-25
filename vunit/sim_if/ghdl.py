4d9aa3d04d3f6ad1043673af9fe339c2837799ad
Lars Asplund <lars.anders.asplund@gmail.com>
2024-02-25 07:55:19 +0100

    Fixes #924

--- vunit/sim_if/ghdl.py
+++ vunit/sim_if/ghdl.py
@@ -391,7 +391,8 @@ class GHDLInterface(SimulatorInterface):  # pylint: disable=too-many-instance-at
         """
         Merge coverage from all test cases
         """
-        output_dir = file_name
+        output_dir = Path(file_name)
+        output_dir.mkdir(parents=True, exist_ok=True)
 
         # Loop over each .gcda output folder and merge them two at a time
         first_input = True
@@ -401,8 +402,8 @@ class GHDLInterface(SimulatorInterface):  # pylint: disable=too-many-instance-at
                     "gcov-tool",
                     "merge",
                     "-o",
-                    output_dir,
-                    coverage_dir if first_input else output_dir,
+                    str(output_dir),
+                    coverage_dir if first_input else str(output_dir),
                     coverage_dir,
                 ]
                 subprocess.call(merge_command)
@@ -411,8 +412,7 @@ class GHDLInterface(SimulatorInterface):  # pylint: disable=too-many-instance-at
                 LOGGER.warning("Missing coverage directory: %s", coverage_dir)
 
         # Find actual output path of the .gcda files (they are deep in hierarchy)
-        dir_path = Path(output_dir)
-        gcda_dirs = {x.parent for x in dir_path.glob("**/*.gcda")}
+        gcda_dirs = {x.parent for x in output_dir.glob("**/*.gcda")}
         assert len(gcda_dirs) == 1, "Expected exactly one folder with gcda files"
         gcda_dir = gcda_dirs.pop()
 
