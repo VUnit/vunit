b2a14e759fe2babd696295554e99bb8228ca99c7
Javier Valverde <javier.valverde.ext@rohde-schwarz.com>
2024-03-13 15:55:34 +0100

    Modify as suggested

--- vunit/vhdl_parser.py
+++ vunit/vhdl_parser.py
@@ -92,7 +92,7 @@ class VHDLDesignFile(object):  # pylint: disable=too-many-instance-attributes
         (?:(?:[a-zA-Z]\w*)\.)?    # Optional library name
         ([a-zA-Z]\w*)\s*          # Capture component name
         (?:generic|port)\s+map\s* # Generic/port map
-        \(.*?\)\s*;               # Open and closing brackets
+        \([\s\w\=\>\,\.\)\(\+\-\'\"\*\/]*\)
         """,
         re.MULTILINE | re.IGNORECASE | re.VERBOSE | re.DOTALL,
     )
