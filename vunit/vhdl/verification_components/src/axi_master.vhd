99b2e5e6da4e0ba6af6274534bb5ef395cdd2407
David Martin <david.martin@phios.group>
2024-05-13 14:57:57 +0200

    Added axi master with equally powerful implementation as axi lite master has

---
+++ vunit/vhdl/verification_components/src/axi_master.vhd
@@ -0,0 +1,225 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this file,
+-- You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Copyright (c) 2014-2024, Lars Asplund lars.anders.asplund@gmail.com
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.axi_master_pkg.all;
+use work.axi_pkg.all;
+use work.axi_slave_private_pkg.check_axi_resp;
+use work.bus_master_pkg.address_length;
+use work.bus_master_pkg.bus_master_t;
+use work.bus_master_pkg.byte_enable_length;
+use work.bus_master_pkg.data_length;
+use work.com_pkg.net;
+use work.com_pkg.receive;
+use work.com_pkg.reply;
+use work.com_types_pkg.all;
+use work.log_levels_pkg.all;
+use work.logger_pkg.all;
+use work.queue_pkg.all;
+use work.sync_pkg.all;
+
+entity axi_master is
+  generic (
+    bus_handle : bus_master_t;
+    drive_invalid : boolean := true;
+    drive_invalid_val : std_logic := 'X'
+  );
+  port (
+    aclk : in std_logic;
+
+    arvalid : out std_logic := '0';
+    arready : in std_logic;
+    arid : out std_logic_vector;
+    araddr : out std_logic_vector(address_length(bus_handle) - 1 downto 0) := (others => '0');
+    arlen : out std_logic_vector;
+    arsize : out std_logic_vector;
+    arburst : out axi_burst_type_t;
+
+    rvalid : in std_logic;
+    rready : out std_logic := '0';
+    rid : in std_logic_vector;
+    rdata : in std_logic_vector(data_length(bus_handle) - 1 downto 0);
+    rresp : in axi_resp_t;
+    rlast : in std_logic;
+
+    awvalid : out std_logic := '0';
+    awready : in std_logic := '0';
+    awid : out std_logic_vector;
+    awaddr : out std_logic_vector(address_length(bus_handle) - 1 downto 0) := (others => '0');
+    awlen : out std_logic_vector;
+    awsize : out std_logic_vector;
+    awburst : out axi_burst_type_t;
+
+    wvalid : out std_logic;
+    wready : in std_logic := '0';
+    wdata : out std_logic_vector(data_length(bus_handle) - 1 downto 0) := (others => '0');
+    wstrb : out std_logic_vector(byte_enable_length(bus_handle) - 1 downto 0) := (others => '0');
+    wlast : out std_logic;
+
+    bvalid : in std_logic;
+    bready : out std_logic := '0';
+    bid : in std_logic_vector;
+    bresp : in axi_resp_t := axi_resp_okay
+  );
+end entity;
+
+architecture a of axi_master is
+  constant reply_queue, message_queue : queue_t := new_queue;
+  signal idle : boolean := true;
+begin
+
+  main : process
+    variable request_msg : msg_t;
+    variable msg_type : msg_type_t;
+  begin
+    receive(net, bus_handle.p_actor, request_msg);
+    msg_type := message_type(request_msg);
+
+    if is_read(msg_type) or is_write(msg_type) then
+      push(message_queue, request_msg);
+    elsif msg_type = wait_until_idle_msg then
+      if not idle or not is_empty(message_queue) then
+        wait until idle and is_empty(message_queue) and rising_edge(aclk);
+      end if;
+      handle_wait_until_idle(net, msg_type, request_msg);
+    else
+      unexpected_msg_type(msg_type);
+    end if;
+  end process;
+
+  -- Use separate process to always align to rising edge of clock
+  bus_process : process
+    procedure drive_ar_invalid is
+    begin
+      if drive_invalid then
+        araddr <= (araddr'range => drive_invalid_val);
+      end if;
+    end procedure;
+
+    procedure drive_aw_invalid is
+    begin
+      if drive_invalid then
+        awaddr <= (awaddr'range => drive_invalid_val);
+      end if;
+    end procedure;
+
+    procedure drive_w_invalid is
+    begin
+      if drive_invalid then
+        wdata <= (wdata'range => drive_invalid_val);
+        wstrb <= (wstrb'range => drive_invalid_val);
+      end if;
+    end procedure;
+
+    variable request_msg : msg_t;
+    variable msg_type : msg_type_t;
+    variable expected_resp : axi_resp_t;
+    variable w_done, aw_done : boolean;
+
+    -- These variables are needed to keep the values for logging when transaction is fully done
+    variable addr_this_transaction : std_logic_vector(awaddr'range) := (others => '0');
+    variable wdata_this_transaction : std_logic_vector(wdata'range) := (others => '0');
+  begin
+    -- Initialization
+    drive_ar_invalid;
+    drive_aw_invalid;
+    drive_w_invalid;
+
+    loop
+      wait until rising_edge(aclk) and not is_empty(message_queue);
+      idle <= false;
+      wait for 0 ps;
+
+      request_msg := pop(message_queue);
+      msg_type := message_type(request_msg);
+
+      if is_read(msg_type) then
+        addr_this_transaction := pop_std_ulogic_vector(request_msg);
+        expected_resp := pop_std_ulogic_vector(request_msg) when is_axi_msg(msg_type) else axi_resp_okay;
+        push(reply_queue, request_msg);
+
+        arvalid <= '1';
+        araddr <= addr_this_transaction;
+        wait until (arvalid and arready) = '1' and rising_edge(aclk);
+        arvalid <= '0';
+        drive_ar_invalid;
+
+        rready <= '1';
+        wait until (rvalid and rready) = '1' and rising_edge(aclk);
+        rready <= '0';
+        check_axi_resp(bus_handle, rresp, expected_resp, "rresp");
+
+        if is_visible(bus_handle.p_logger, debug) then
+          debug(bus_handle.p_logger,
+                "Read 0x" & to_hstring(rdata) &
+                  " from address 0x" & to_hstring(addr_this_transaction));
+        end if;
+
+      elsif is_write(msg_type) then
+        addr_this_transaction := pop_std_ulogic_vector(request_msg);
+        wdata_this_transaction := pop_std_ulogic_vector(request_msg);
+        wstrb <= pop_std_ulogic_vector(request_msg);
+        expected_resp := pop_std_ulogic_vector(request_msg) when is_axi_msg(msg_type) else axi_resp_okay;
+        delete(request_msg);
+
+        awaddr <= addr_this_transaction;
+        wdata <= wdata_this_transaction;
+
+        wvalid <= '1';
+        awvalid <= '1';
+
+        w_done := false;
+        aw_done := false;
+        while not (w_done and aw_done) loop
+          wait until ((awvalid and awready) = '1' or (wvalid and wready) = '1') and rising_edge(aclk);
+
+          if (awvalid and awready) = '1' then
+            awvalid <= '0';
+            drive_aw_invalid;
+
+            aw_done := true;
+          end if;
+
+          if (wvalid and wready) = '1' then
+            wvalid <= '0';
+            drive_w_invalid;
+
+            w_done := true;
+          end if;
+        end loop;
+
+        bready <= '1';
+        wait until (bvalid and bready) = '1' and rising_edge(aclk);
+        bready <= '0';
+        check_axi_resp(bus_handle, bresp, expected_resp, "bresp");
+
+        if is_visible(bus_handle.p_logger, debug) then
+          debug(bus_handle.p_logger,
+                "Wrote 0x" & to_hstring(wdata_this_transaction) &
+                  " to address 0x" & to_hstring(addr_this_transaction));
+        end if;
+      end if;
+
+      idle <= true;
+    end loop;
+  end process;
+
+  -- Reply in separate process do not destroy alignment with the clock
+  read_reply : process
+    variable request_msg, reply_msg : msg_t;
+  begin
+    wait until (rvalid and rready) = '1' and rising_edge(aclk);
+    request_msg := pop(reply_queue);
+    reply_msg := new_msg;
+    push_std_ulogic_vector(reply_msg, rdata);
+    reply(net, request_msg, reply_msg);
+    delete(request_msg);
+  end process;
+
+end architecture;
---
+++ vunit/vhdl/verification_components/test/tb_axi_master.vhd
@@ -0,0 +1,507 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this file,
+-- You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Copyright (c) 2014-2024, Lars Asplund lars.anders.asplund@gmail.com
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+context work.com_context;
+
+use work.axi_pkg.all;
+use work.bus_master_pkg.all;
+use work.axi_master_pkg.all;
+
+library osvvm;
+use osvvm.RandomPkg.all;
+
+entity tb_axi_master is
+  generic (runner_cfg : string);
+end entity;
+
+architecture a of tb_axi_master is
+  constant num_random_tests : integer := 128;
+
+  signal clk    : std_logic := '0';
+
+  signal arvalid : std_logic;
+  signal arready : std_logic := '0';
+  signal arid    : std_logic_vector(7 downto 0); --TBD
+  signal araddr  : std_logic_vector(31 downto 0);
+  signal arlen   : std_logic_vector(7 downto 0); --TBD
+  signal arsize  : std_logic_vector(7 downto 0); --TBD
+  signal arburst : axi_burst_type_t; --TBD
+
+  signal rvalid  : std_logic;
+  signal rready  : std_logic := '0';
+  signal rid     : std_logic_vector(7 downto 0); --TBD
+  signal rdata   : std_logic_vector(15 downto 0);
+  signal rresp   : std_logic_vector(1 downto 0);
+  signal rlast   : std_logic;
+
+  signal awvalid : std_logic;
+  signal awready : std_logic := '0';
+  signal awid    : std_logic_vector(7 downto 0); --TBD
+  signal awaddr  : std_logic_vector(31 downto 0);
+  signal awlen   : std_logic_vector(7 downto 0); --TBD
+  signal awsize  : std_logic_vector(7 downto 0); --TBD
+  signal awburst : axi_burst_type_t; --TBD
+
+  signal wvalid  : std_logic;
+  signal wready  : std_logic := '0';
+  signal wdata   : std_logic_vector(15 downto 0);
+  signal wstrb   : std_logic_vector(1 downto 0);
+  signal wlast   : std_logic;
+
+  signal bvalid  : std_logic := '0';
+  signal bready  : std_logic;
+  signal bid     : std_logic_vector(7 downto 0); --TBD
+  signal bresp   : std_logic_vector(1 downto 0);
+
+  constant bus_handle : bus_master_t := new_bus(data_length => wdata'length,
+                                                address_length => awaddr'length);
+
+  signal start, done : boolean := false;
+begin
+
+  main : process
+    variable tmp : std_logic_vector(rdata'range);
+    variable rnd : RandomPType;
+    variable timestamp : time;
+  begin
+    test_runner_setup(runner, runner_cfg);
+    rnd.InitSeed("common_seed");
+    start <= true;
+    wait for 0 ns;
+
+    if run("Test single write") then
+      mock(get_logger(bus_handle), debug);
+      write_bus(net, bus_handle, x"01234567", x"1122");
+      wait_until_idle(net, bus_handle);
+      check_only_log(get_logger(bus_handle), "Wrote 0x1122 to address 0x01234567", debug);
+      unmock(get_logger(bus_handle));
+
+    elsif run("Test single write with byte enable") then
+      write_bus(net, bus_handle, x"01234567", x"1122", byte_enable => "10");
+
+    elsif run("Test write not okay") then
+      write_bus(net, bus_handle, x"01234567", x"1122");
+
+    elsif run("Test write with axi resp") then
+      write_axi(net, bus_handle, x"01234567", x"1122", axi_resp_slverr);
+
+    elsif run("Test write with wrong axi resp") then
+      write_axi(net, bus_handle, x"01234567", x"1122", axi_resp_decerr);
+
+    elsif run("Test single read") then
+      mock(get_logger(bus_handle), debug);
+      read_bus(net, bus_handle, x"01234567", tmp);
+      check_equal(tmp, std_logic_vector'(x"5566"), "read data");
+      check_only_log(get_logger(bus_handle), "Read 0x5566 from address 0x01234567", debug);
+      unmock(get_logger(bus_handle));
+
+    elsif run("Test read not okay") then
+      read_bus(net, bus_handle, x"01234567", tmp);
+
+    elsif run("Test read with axi resp") then
+      read_axi(net, bus_handle, x"01234567", axi_resp_slverr, tmp);
+
+    elsif run("Test read with wrong axi resp") then
+      read_axi(net, bus_handle, x"01234567", axi_resp_exokay, tmp);
+
+    elsif run("Test random") then
+      for i in 0 to num_random_tests-1 loop
+        if rnd.RandInt(0, 1) = 0 then
+          read_bus(net, bus_handle, rnd.RandSlv(araddr'length), tmp);
+          check_equal(tmp, rnd.RandSlv(rdata'length), "read data");
+        else
+          write_bus(net, bus_handle, rnd.RandSlv(awaddr'length), rnd.RandSlv(wdata'length));
+        end if;
+      end loop;
+
+    elsif run("Test random axi resp") then
+      for i in 0 to num_random_tests-1 loop
+        if rnd.RandInt(0, 1) = 0 then
+          read_axi(net, bus_handle, rnd.RandSlv(araddr'length), rnd.RandSlv(axi_resp_t'length), tmp);
+          check_equal(tmp, rnd.RandSlv(rdata'length), "read data");
+        else
+          write_axi(net, bus_handle, rnd.RandSlv(awaddr'length), rnd.RandSlv(wdata'length),
+                         rnd.RandSlv(axi_resp_t'length));
+        end if;
+      end loop;
+
+    elsif run("Test idle when idle") then
+      wait until rising_edge(clk);
+      write_bus(net, bus_handle, x"01234567", x"1122");
+      wait for 0 ps;
+      timestamp := now;
+      wait_until_idle(net, bus_handle);
+      check(now > timestamp, "Write: First wait did not have to wait");
+      timestamp := now;
+      wait_until_idle(net, bus_handle);
+      check_equal(timestamp, now, "Write: Second wait had to wait");
+
+      wait until rising_edge(clk);
+      read_bus(net, bus_handle, x"01234567", tmp);
+      timestamp := now;
+      wait_until_idle(net, bus_handle);
+      check_equal(timestamp, now, "Read: Second wait had to wait");
+
+    end if;
+
+    wait for 100 ns;
+
+    if not done then
+      wait until done;
+    end if;
+
+    test_runner_cleanup(runner);
+  end process;
+  test_runner_watchdog(runner, 100 us);
+
+
+  support : process
+    variable rnd : RandomPType;
+  begin
+    rnd.InitSeed("common_seed");
+
+    wait until start;
+
+    if enabled("Test single write") then
+      awready <= '1';
+      wait until (awready and awvalid) = '1' and rising_edge(clk);
+      awready <= '0';
+      check_equal(awaddr, std_logic_vector'(x"01234567"), "awaddr");
+
+      wready <= '1';
+      wait until (wready and wvalid) = '1' and rising_edge(clk);
+      wready <= '0';
+      check_equal(wdata, std_logic_vector'(x"1122"), "wdata");
+      check_equal(wstrb, std_logic_vector'("11"), "wstrb");
+
+      bvalid <= '1';
+      bresp <= axi_resp_okay;
+      wait until (bready and bvalid) = '1' and rising_edge(clk);
+      bvalid <= '0';
+
+      done <= true;
+
+    elsif enabled("Test single write with byte enable") then
+      awready <= '1';
+      wait until (awready and awvalid) = '1' and rising_edge(clk);
+      awready <= '0';
+      check_equal(awaddr, std_logic_vector'(x"01234567"), "awaddr");
+
+      wready <= '1';
+      wait until (wready and wvalid) = '1' and rising_edge(clk);
+      wready <= '0';
+      check_equal(wdata, std_logic_vector'(x"1122"), "wdata");
+      check_equal(wstrb, std_logic_vector'("10"), "wstrb");
+
+      bvalid <= '1';
+      bresp <= axi_resp_okay;
+      wait until (bready and bvalid) = '1' and rising_edge(clk);
+      bvalid <= '0';
+
+      done <= true;
+
+    elsif enabled("Test write not okay") then
+      awready <= '1';
+      wait until (awready and awvalid) = '1' and rising_edge(clk);
+      awready <= '0';
+
+      wready <= '1';
+      wait until (wready and wvalid) = '1' and rising_edge(clk);
+      wready <= '0';
+
+      bvalid <= '1';
+      bresp <= axi_resp_slverr;
+      mock(bus_logger, failure);
+      wait until (bready and bvalid) = '1' and rising_edge(clk);
+      bvalid <= '0';
+      wait until mock_queue_length > 0 for 0 ns;
+      check_only_log(bus_logger, "bresp - Got AXI response SLVERR(10) expected OKAY(00)", failure);
+      unmock(bus_logger);
+
+      done <= true;
+
+    elsif enabled("Test write with axi resp") then
+      awready <= '1';
+      wait until (awready and awvalid) = '1' and rising_edge(clk);
+      awready <= '0';
+      check_equal(awaddr, std_logic_vector'(x"01234567"), "awaddr");
+
+      wready <= '1';
+      wait until (wready and wvalid) = '1' and rising_edge(clk);
+      wready <= '0';
+      check_equal(wdata, std_logic_vector'(x"1122"), "wdata");
+      check_equal(wstrb, std_logic_vector'("11"), "wstrb");
+
+      bvalid <= '1';
+      bresp <= axi_resp_slverr;
+      wait until (bready and bvalid) = '1' and rising_edge(clk);
+      bvalid <= '0';
+
+      done <= true;
+
+    elsif enabled("Test write with wrong axi resp") then
+      awready <= '1';
+      wait until (awready and awvalid) = '1' and rising_edge(clk);
+      awready <= '0';
+      check_equal(awaddr, std_logic_vector'(x"01234567"), "awaddr");
+
+      wready <= '1';
+      wait until (wready and wvalid) = '1' and rising_edge(clk);
+      wready <= '0';
+      check_equal(wdata, std_logic_vector'(x"1122"), "wdata");
+      check_equal(wstrb, std_logic_vector'("11"), "wstrb");
+
+      bvalid <= '1';
+      bresp <= axi_resp_exokay;
+      mock(bus_logger, failure);
+      wait until (bready and bvalid) = '1' and rising_edge(clk);
+      bvalid <= '0';
+      wait until mock_queue_length > 0 for 0 ns;
+      check_only_log(bus_logger, "bresp - Got AXI response EXOKAY(01) expected DECERR(11)", failure);
+      unmock(bus_logger);
+
+      done <= true;
+
+    elsif enabled("Test single read") then
+      arready <= '1';
+      wait until (arready and arvalid) = '1' and rising_edge(clk);
+      arready <= '0';
+      check_equal(araddr, std_logic_vector'(x"01234567"), "araddr");
+
+      rvalid <= '1';
+      rresp <= axi_resp_okay;
+      rdata <= x"5566";
+      wait until (rready and rvalid) = '1' and rising_edge(clk);
+      rvalid <= '0';
+
+      done <= true;
+
+    elsif enabled("Test read not okay") then
+      arready <= '1';
+      wait until (arready and arvalid) = '1' and rising_edge(clk);
+      arready <= '0';
+
+      rvalid <= '1';
+      rresp <= axi_resp_decerr;
+      rdata <= x"5566";
+      mock(bus_logger, failure);
+      wait until (rready and rvalid) = '1' and rising_edge(clk);
+      rvalid <= '0';
+      wait until mock_queue_length > 0 for 0 ns;
+      check_only_log(bus_logger, "rresp - Got AXI response DECERR(11) expected OKAY(00)", failure);
+      unmock(bus_logger);
+
+      done <= true;
+
+    elsif enabled("Test read with axi resp") then
+      arready <= '1';
+      wait until (arready and arvalid) = '1' and rising_edge(clk);
+      arready <= '0';
+      check_equal(araddr, std_logic_vector'(x"01234567"), "araddr");
+
+      rvalid <= '1';
+      rresp <= axi_resp_slverr;
+      rdata <= x"0000";
+      wait until (rready and rvalid) = '1' and rising_edge(clk);
+      rvalid <= '0';
+
+      done <= true;
+
+    elsif enabled("Test read with wrong axi resp") then
+      arready <= '1';
+      wait until (arready and arvalid) = '1' and rising_edge(clk);
+      arready <= '0';
+      check_equal(araddr, std_logic_vector'(x"01234567"), "araddr");
+
+      rvalid <= '1';
+      rresp <= axi_resp_decerr;
+      rdata <= x"0000";
+      mock(bus_logger, failure);
+      wait until (rready and rvalid) = '1' and rising_edge(clk);
+      rvalid <= '0';
+      wait until mock_queue_length > 0 for 0 ns;
+      check_only_log(bus_logger, "rresp - Got AXI response DECERR(11) expected EXOKAY(01)", failure);
+      unmock(bus_logger);
+
+      done <= true;
+
+    elsif enabled("Test random") then
+      for i in 0 to num_random_tests-1 loop
+        if rnd.RandInt(0, 1) = 0 then
+          arready <= '1';
+          wait until (arready and arvalid) = '1' and rising_edge(clk);
+          arready <= '0';
+          check_equal(araddr, rnd.RandSlv(araddr'length), "araddr");
+
+          rvalid <= '1';
+          rresp <= axi_resp_okay;
+          rdata <= rnd.RandSlv(rdata'length);
+          wait until (rready and rvalid) = '1' and rising_edge(clk);
+          rvalid <= '0';
+        else
+          awready <= '1';
+          wait until (awready and awvalid) = '1' and rising_edge(clk);
+          awready <= '0';
+          check_equal(awaddr, rnd.RandSlv(awaddr'length), "awaddr");
+
+          wready <= '1';
+          wait until (wready and wvalid) = '1' and rising_edge(clk);
+          wready <= '0';
+          check_equal(wdata, rnd.RandSlv(wdata'length), "wdata");
+          check_equal(wstrb, std_logic_vector'("11"), "wstrb");
+
+          bvalid <= '1';
+          bresp <= axi_resp_okay;
+          wait until (bready and bvalid) = '1' and rising_edge(clk);
+          bvalid <= '0';
+        end if;
+      end loop;
+      done <= true;
+
+    elsif enabled("Test random axi resp") then
+      for i in 0 to num_random_tests-1 loop
+        if rnd.RandInt(0, 1) = 0 then
+          arready <= '1';
+          wait until (arready and arvalid) = '1' and rising_edge(clk);
+          arready <= '0';
+          check_equal(araddr, rnd.RandSlv(araddr'length), "araddr");
+
+          rvalid <= '1';
+          rresp <= rnd.RandSlv(axi_resp_t'length);
+          rdata <= rnd.RandSlv(rdata'length);
+          wait until (rready and rvalid) = '1' and rising_edge(clk);
+          rvalid <= '0';
+        else
+          awready <= '1';
+          wait until (awready and awvalid) = '1' and rising_edge(clk);
+          awready <= '0';
+          check_equal(awaddr, rnd.RandSlv(awaddr'length), "awaddr");
+
+          wready <= '1';
+          wait until (wready and wvalid) = '1' and rising_edge(clk);
+          wready <= '0';
+          check_equal(wdata, rnd.RandSlv(wdata'length), "wdata");
+          check_equal(wstrb, std_logic_vector'("11"), "wstrb");
+
+          bvalid <= '1';
+          bresp <= rnd.RandSlv(axi_resp_t'length);
+          wait until (bready and bvalid) = '1' and rising_edge(clk);
+          bvalid <= '0';
+        end if;
+      end loop;
+      done <= true;
+
+    elsif enabled("Test idle when idle") then
+      wait until rising_edge(clk);
+      awready <= '1';
+      wait until (awready and awvalid) = '1' and rising_edge(clk);
+      awready <= '0';
+      check_equal(awaddr, std_logic_vector'(x"01234567"), "awaddr");
+
+      wait until rising_edge(clk);
+      wready <= '1';
+      wait until (wready and wvalid) = '1' and rising_edge(clk);
+      wready <= '0';
+      check_equal(wdata, std_logic_vector'(x"1122"), "wdata");
+      check_equal(wstrb, std_logic_vector'("11"), "wstrb");
+
+      wait until rising_edge(clk);
+      bvalid <= '1';
+      bresp <= axi_resp_okay;
+      wait until (bready and bvalid) = '1' and rising_edge(clk);
+      bvalid <= '0';
+
+      wait until rising_edge(clk);
+      arready <= '1';
+      wait until (arready and arvalid) = '1' and rising_edge(clk);
+      arready <= '0';
+      check_equal(araddr, std_logic_vector'(x"01234567"), "araddr");
+
+      wait until rising_edge(clk);
+      rvalid <= '1';
+      rresp <= axi_resp_okay;
+      rdata <= x"5566";
+      wait until (rready and rvalid) = '1' and rising_edge(clk);
+      rvalid <= '0';
+
+      done <= true;
+
+    end if;
+  end process;
+
+  check_not_valid : process
+    constant a_addr_invalid_value : std_logic_vector(araddr'range) := (others => 'X');
+    constant wdata_invalid_value : std_logic_vector(wdata'range) := (others => 'X');
+    constant wstrb_invalid_value : std_logic_vector(wstrb'range) := (others => 'X');
+  begin
+    wait until rising_edge(clk);
+
+    -- All signals should be driven with 'X' when the channel is not valid
+    -- (R and B channels have no outputs from the VC, except for handshake).
+
+    if not arvalid then
+      check_equal(araddr, a_addr_invalid_value, "ARADDR not X when ARVALID low");
+    end if;
+
+    if not awvalid then
+      check_equal(awaddr, a_addr_invalid_value, "AWADDR not X when AWVALID low");
+    end if;
+
+    if not wvalid then
+      check_equal(wdata, wdata_invalid_value, "WDATA not X when WVALID low");
+      check_equal(wstrb, wstrb_invalid_value, "WSTRB not X when WVALID low");
+    end if;
+  end process;
+
+  dut : entity work.axi_master
+    generic map (
+      bus_handle => bus_handle)
+    port map (
+      aclk    => clk,
+
+      arvalid => arvalid,
+      arready => arready,
+      arid => arid,
+      araddr => araddr,
+      arlen => arlen,
+      arsize => arsize,
+      arburst => arburst,
+  
+      rvalid => rvalid,
+      rready => rready,
+      rid => rid,
+      rdata => rdata,
+      rresp => rresp,
+      rlast => rlast,
+  
+      awvalid => awvalid,
+      awready => awready,
+      awid => awid,
+      awaddr => awaddr,
+      awlen => awlen,
+      awsize => awsize,
+      awburst => awburst,
+  
+      wvalid => wvalid,
+      wready => wready,
+      wdata => wdata,
+      wstrb => wstrb,
+      wlast => wlast,
+  
+      bvalid => bvalid,
+      bready => bready,
+      bid => bid,
+      bresp => bresp);
+
+  clk <= not clk after 5 ns;
+
+end architecture;
