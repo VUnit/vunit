1759b69671c730706922507fd5aea71da9919a4e
Lars Asplund <lars.anders.asplund@gmail.com>
2026-01-03 10:21:24 +0100

    Updated license to 2026.

--- vunit/vhdl/logging/src/logger_pkg-body.vhd
+++ vunit/vhdl/logging/src/logger_pkg-body.vhd
@@ -2,7 +2,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this file,
 -- You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- Copyright (c) 2014-2025, Lars Asplund lars.anders.asplund@gmail.com
+-- Copyright (c) 2014-2026, Lars Asplund lars.anders.asplund@gmail.com
 
 package body logger_pkg is
   constant global_log_count : integer_vector_ptr_t := new_integer_vector_ptr(1, value => 0);
