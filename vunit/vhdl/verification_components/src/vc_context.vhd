2be2cf6bb9cbad0c815db0a07a76349d067ef802
Lars Asplund <lars.anders.asplund@gmail.com>
2026-01-05 12:10:17 +0100

    Updated contexts to include missing packages.

--- vunit/vhdl/verification_components/src/vc_context.vhd
+++ vunit/vhdl/verification_components/src/vc_context.vhd
@@ -6,20 +6,23 @@
 
 context vc_context is
   library vunit_lib;
+  context vunit_lib.com_context;
+
   use vunit_lib.avalon_pkg.all;
   use vunit_lib.avalon_stream_pkg.all;
-  use vunit_lib.bus_master_pkg.all;
+  use vunit_lib.axi_lite_master_pkg.all;
   use vunit_lib.axi_pkg.all;
   use vunit_lib.axi_slave_pkg.all;
   use vunit_lib.axi_statistics_pkg.all;
   use vunit_lib.axi_stream_pkg.all;
+  use vunit_lib.bus_master_pkg.all;
   use vunit_lib.memory_pkg.all;
   use vunit_lib.memory_utils_pkg.all;
+  use vunit_lib.signal_checker_pkg.all;
   use vunit_lib.stream_master_pkg.all;
   use vunit_lib.stream_slave_pkg.all;
   use vunit_lib.sync_pkg.all;
   use vunit_lib.uart_pkg.all;
   use vunit_lib.vc_pkg.all;
   use vunit_lib.wishbone_pkg.all;
-  context vunit_lib.com_context;
 end context;
