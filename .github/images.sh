cff147a787d8afba9bbdeb943ce01e37ae3325f0
Lars Asplund <lars.anders.asplund@gmail.com>
2026-02-14 22:34:59 +0100

    Drop support for Python 3.8 and 3.9 and start running tests with Python 3.14.

--- .github/images.sh
+++ .github/images.sh
@@ -4,15 +4,17 @@
 set -e
 
 docker build \
-  --build-arg IMAGE="python:3-slim-bullseye" \
-  --build-arg LLVM_VER=9 \
-  --build-arg GNAT_VER=9 \
+  --build-arg IMAGE="python:3-slim-bookworm" \
+  --build-arg LLVM_VER=14 \
+  --build-arg GNAT_VER=12 \
   --target vunit \
   -t "vunit/dev/${TAG}" \
   - <<-EOF
 $(curl -fsSL https://raw.githubusercontent.com/ghdl/docker/master/run_debian.dockerfile)
 
+FROM ghdl/pkg:bookworm-$PKG AS ghdl
+
 FROM $TAG AS vunit
-COPY --from=ghdl/pkg:bullseye-$PKG / /
+COPY --from=ghdl /usr/ /usr/
 RUN pip install -U tox colorama coverage --progress-bar off
 EOF
