119f9ace6d651f6c11fae57972268f7726c4442d
Ronny Nilsson <31500116+roynil@users.noreply.github.com>
2026-08-27 10:04:35 +0200

    More stable code
    Prevents getting:
    # ** Error: error reading "stdin": interrupted system call
    
    # Error in macro /home/jenkins/.local/lib/python3.6/site-packages/vunit/sim_if/tcl_read_eval_loop.tcl line 12
    
    # error reading "stdin": interrupted system call 
    
    when running regression from Jenkins

--- vunit/sim_if/tcl_read_eval_loop.tcl
+++ vunit/sim_if/tcl_read_eval_loop.tcl
@@ -5,7 +5,30 @@
 # Copyright (c) 2014-2023, Lars Asplund lars.anders.asplund@gmail.com
 
 while {1} {
-    set line [gets stdin]
+    # catch returns 1 on error, 0 on success.
+    # On error, 'line' holds the error message. On success, it holds the read string.
+    if {[catch {gets stdin} line]} {
+        # If the OS interrupted the read, just try again
+        if {[string match "*interrupted system call*" $line]} {
+            continue
+        } else {
+            # Break on any other fatal error
+            puts "Fatal error reading stdin: $line"
+            break
+        }
+    }
+    
+    # Check if the Python parent process closed the pipe (EOF)
+    if {[eof stdin]} {
+        break
+    }
+    
+    # Skip empty lines to prevent unnecessary eval overhead
+    if {$line eq ""} {
+        continue
+    }
+
+    # Evaluate the command sent from Python
     if {[catch {eval $line} error_msg]} {
         puts "$line - $error_msg"
     }
